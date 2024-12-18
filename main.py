14632bddace9eb6ff7936a8b6f73c8a6e130e396
stfmarkov <stf.markov@gmail.com>
2024-12-18 19:15:26 +0200

    object grouping for easier loop management

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while (True):
@@ -20,11 +25,13 @@ def main():
                 return
             
         pygame.Surface.fill(screen, (0,0,0))
+    
+        for object in updatable:
+            object.update(dt)
 
-    
-        player.update(dt)
-        player.draw(screen)
-    
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
 
         passedTime = clock.tick(60)
